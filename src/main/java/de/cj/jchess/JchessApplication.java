c0d967389cc9981f189dc21c55378358a1097cb9
tophernk <topher.nk@googlemail.com>
2021-01-07 20:30:57 +0100

    init

---
+++ src/main/java/de/cj/jchess/JchessApplication.java
@@ -0,0 +1,13 @@
+package de.cj.jchess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JchessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JchessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/cj/jchess/JchessApplicationTests.java
@@ -0,0 +1,13 @@
+package de.cj.jchess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JchessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
