9634af2a1924e2d1a7f6a463f968e8b51c4f89ef
tophernk <topher.nk@googlemail.com>
2021-02-11 16:58:52 +0100

    execute move

--- src/test/java/de/cj/jchess/service/ChessMoveServiceTest.java
+++ src/test/java/de/cj/jchess/service/ChessMoveServiceTest.java
@@ -28,7 +28,8 @@ class ChessMoveServiceTest {
                 .pieceType(ChessPieceType.PAWN)
                 .pieceColor(ChessPieceColor.WHITE)
                 .position(ChessPosition.E2)
-                .availablePositions(Stream.of(ChessPosition.E3, ChessPosition.E4).collect(Collectors.toSet()))
+                .availablePositions(Stream.of(ChessPosition.E3, ChessPosition.E4)
+                        .collect(Collectors.toSet()))
                 .build();
         ChessConfiguration configuration = ChessConfiguration.builder()
                 .whitePieces(Set.of(whitePawn))
@@ -50,7 +51,43 @@ class ChessMoveServiceTest {
                 .isNotLongCastlesWhite()
                 .isNotShortCastlesBlack()
                 .isNotShortCastlesWhite();
-        assertThat(whitePawn).hasOnlyAvailablePositions(ChessPosition.E5);
+    }
+
+    @Test
+    void executeMoveWithTakes() {
+        ChessPiece blackPawn = ChessPiece.builder()
+                .pieceType(ChessPieceType.PAWN)
+                .pieceColor(ChessPieceColor.BLACK)
+                .position(ChessPosition.D3)
+                .availablePositions(Stream.of(ChessPosition.E2, ChessPosition.D2).collect(Collectors.toSet()))
+                .build();
+        ChessPiece whitePawn = ChessPiece.builder()
+                .pieceType(ChessPieceType.PAWN)
+                .pieceColor(ChessPieceColor.WHITE)
+                .position(ChessPosition.E2)
+                .availablePositions(Stream.of(ChessPosition.E3, ChessPosition.E4, ChessPosition.D3).collect(Collectors.toSet()))
+                .build();
+        ChessConfiguration configuration = ChessConfiguration.builder()
+                .whitePieces(Stream.of(whitePawn).collect(Collectors.toSet()))
+                .blackPieces(Stream.of(blackPawn).collect(Collectors.toSet()))
+                .longCastlesBlack(false)
+                .longCastlesWhite(false)
+                .shortCastlesBlack(false)
+                .shortCastlesWhite(false)
+                .checkBlack(false)
+                .checkWhite(false)
+                .build();
+        boolean moveExecuted = chessMoveService.executeMove(configuration, ChessPosition.E2, ChessPosition.D3);
+        assertTrue(moveExecuted);
+        assertThat(configuration).hasOnlyWhitePieces(whitePawn)
+                .hasNoBlackPieces()
+                .isNotCheckBlack()
+                .isNotCheckWhite()
+                .isNotLongCastlesBlack()
+                .isNotLongCastlesWhite()
+                .isNotShortCastlesBlack()
+                .isNotShortCastlesWhite();
+        assertThat(whitePawn).hasOnlyAvailablePositions(ChessPosition.D4);
     }
 
     @Test
